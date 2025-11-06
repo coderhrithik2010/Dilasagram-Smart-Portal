edde1c432a81c92ad3048c4157a21665da150d6e
coderhrithik2010 <raistarhrithik@gmail.com>
2025-11-06 15:32:13 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+// Firebase Setup for DCS Smart Portal
+const firebaseConfig = {
+    apiKey: "AIzaSyAqWfVIxOvP7vcV96n0as5FwDovhccTtLE",
+    authDomain: "smart-school-dilasagram.firebaseapp.com",
+    projectId: "smart-school-dilasagram",
+    storageBucket: "smart-school-dilasagram.firebasestorage.app",
+    messagingSenderId: "646237863186",
+    appId: "1:646237863186:web:2d95dd8c983625f67587cb"
+  };
